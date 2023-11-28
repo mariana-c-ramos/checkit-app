6333ac5fdc83ee0230573e8494d9a74f4a1d18dc
Mariana Ramos <mary.programms@gmail.com>
2023-11-28 13:00:06 +0000

    create main route

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,7 +1,8 @@
 const tasksModel = require('../models/tasksModel');
 
-const getAll = (req,res) => {
-    return res.status(200).json({ message : 'Controller working.'})
+const getAll = async (req,res) => {
+    const tasks = await tasksModel.getAll();
+    return res.status(200).json(tasks)
 };
 
 module.exports = {
--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -1,10 +1,16 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-    const tasks = await connection.execute('SELECT * FROM tasks');
+    const [tasks] = await connection.execute('SELECT * FROM tasks');
     return tasks;
 };
 
+const createTask = async (task) => {
+    const { title } = task;
+    const createdTask = await connection.execute('INSERT INTO tasks(title, status, created_at) VALUES (?,?,?)');
+}
+
 module.exports = {
-    getAll
+    getAll,
+    createTask
 };
