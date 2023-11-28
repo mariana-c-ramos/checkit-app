3f3ea90ef970ff8a4e355a692810975b45d2906c
Mariana Ramos <mary.programms@gmail.com>
2023-11-28 14:58:14 +0000

    createTask, deleteTask

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -7,10 +7,19 @@ const getAll = async () => {
 
 const createTask = async (task) => {
     const { title } = task;
-    const createdTask = await connection.execute('INSERT INTO tasks(title, status, created_at) VALUES (?,?,?)');
+    const dateUTC = new Date(Date.now()).toUTCString();
+    const query = 'INSERT INTO tasks(title, status, created_at) VALUES (?,?,?)';
+    const createdTask = await connection.execute(query, [title, 'pending', dateUTC]);
+    return {insertId: createdTask.insertId}
+};
+
+const deleteTask = async (id) => {
+    const removedTask = await connection.execute('DELETE FROM tasks WHERE id = ?', [id]);
+    return removedTask;
 }
 
 module.exports = {
     getAll,
-    createTask
+    createTask,
+    deleteTask
 };
