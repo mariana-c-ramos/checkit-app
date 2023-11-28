6b29db24cd72fcfabd8b42a9426be417e07e81e4
Mariana Ramos <mary.programms@gmail.com>
2023-11-28 15:23:52 +0000

    delete and update task validation

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -8,17 +8,25 @@ const getAll = async (req,res) => {
 const createTask = async (req,res) => {
     const createdTask = await tasksModel.createTask(req.body);
     return res.status(201).json(createdTask)
-}
+};
 
 const deleteTask = async (req,res) => {
     const { id } = req.params;
 
     await tasksModel.deleteTask(id);
     return res.status(204).json();
-}
+};
+
+const updateTask = async (req,res) => {
+    const { id } = req.params;
+    await tasksModel.updateTask(id, req.body);
+    return res.status(204).json();
+
+};
 
 module.exports = {
     getAll,
     createTask,
-    deleteTask
+    deleteTask,
+    updateTask
 };
